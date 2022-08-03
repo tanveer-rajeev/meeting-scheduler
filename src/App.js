55f579fbf5ede5086138752982728cb2c23a1479
rajeeb <rajeeb@bitmascot.com>
2022-08-03 12:32:07 +0600

    react-scripts-version change

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import "./App.css";
 import Login from "./Components/Credential/Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
-import { createContext, useState } from "react";
+import react, { createContext, useState } from "react";
 import Home from "./Components/Home/Home";
 import UserScheduler from "./Components/User/UserScheduler";
 import DisplayRooms from "./Components/Room/DisplayRooms";
 import CreateRoom from "./Components/Room/CreateRoom";
-import "./App.css";
 import UserProfile from "./Components/User/UserProfile";
 
 export const UserContext = createContext();
